967251f9814d4f97bcb7d36b9a5a5eb0626f09b7
DimitriStintzi <dimitri.stintzi@student.junia.com>
2024-05-12 13:23:08 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+// Sélectionnez les éléments triangulaires
+const upTriangle = document.getElementById("up");
+const rightTriangle = document.getElementById("right");
+const downTriangle = document.getElementById("down");
+const leftTriangle = document.getElementById("left");
+
+// Ajoutez des gestionnaires d'événements de clic à chaque triangle
+upTriangle.addEventListener("click", () => {
+    upTriangle.style.borderBottomColor = "blue"; // Changez seulement la couleur de la bordure inférieure
+
+    // Remettre la couleur rouge après 2 secondes
+    setTimeout(() => {
+        upTriangle.style.borderBottomColor = "red";
+    }, 300);
+});
+
+rightTriangle.addEventListener("click", () => {
+    rightTriangle.style.borderLeftColor = "blue"; // Changez seulement la couleur de la bordure gauche
+
+    // Remettre la couleur rouge après 2 secondes
+    setTimeout(() => {
+        rightTriangle.style.borderLeftColor = "red";
+    }, 300);
+});
+
+downTriangle.addEventListener("click", () => {
+    downTriangle.style.borderTopColor = "blue"; // Changez seulement la couleur de la bordure supérieure
+
+    // Remettre la couleur rouge après 2 secondes
+    setTimeout(() => {
+        downTriangle.style.borderTopColor = "red";
+    }, 300);
+});
+
+leftTriangle.addEventListener("click", () => {
+    leftTriangle.style.borderRightColor = "blue"; // Changez seulement la couleur de la bordure droite
+
+    // Remettre la couleur rouge après 2 secondes
+    setTimeout(() => {
+        leftTriangle.style.borderRightColor = "red";
+    }, 300);
+});
